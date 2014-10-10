99b223f084754b310dbf91fe442d3754f8b155a8
Jae Won Choi <jaewonch@ece003.ece.cmu.edu>
2014-10-09 22:00:33 -0400

    changed it

--- usbHost.sv
+++ usbHost.sv
@@ -1,3 +1,5 @@
+`define TOKEN 2'b1
+
 // Write your usb host here.  Do not modify the port list.
 module usbHost
   (input logic clk, rst_L, 
@@ -13,12 +15,19 @@ module usbHost
   /***************************************/
 
   /* Tasks needed to be finished to run testbenches */
-
+/*
   task prelabRequest
   // sends an OUT packet with ADDR=5 and ENDP=4
   // packet should have SYNC and EOP too
-  (input bit  [7:0] data);
-    
+  (input bit [7:0] data);
+	
+	wait(free_inbound);
+	pkt_type <= 'TOKEN;
+	token <= 24'b0111_1000_101_0000_0010_01100;
+	wait(pkt_received);
+
+	wait(free_inbound);
+
   endtask: prelabRequest
 
   task readData
@@ -38,7 +47,7 @@ module usbHost
    output bit        success);
 
   endtask: writeData
-
+*/
   // usbHost starts here!!
 
   /****Bit stream encoder****/
@@ -94,10 +103,41 @@ module usbHost
   
   /***********************************/
   
+  /***************************************/
 
+  /* Tasks needed to be finished to run testbenches */
 
+  task prelabRequest
+  // sends an OUT packet with ADDR=5 and ENDP=4
+  // packet should have SYNC and EOP too
+  (input bit [7:0] data);
+	
+	wait(free_inbound);
+	pkt_type <= `TOKEN;
+	token <= 24'b0111_1000_101_0000_0010_01100;
+	wait(pkt_received);
 
-  
-  
+	wait(free_inbound);
+
+  endtask: prelabRequest
+
+  task readData
+  // host sends memPage to thumb drive and then gets data back from it
+  // then returns data and status to the caller
+  (input  bit [15:0]  mempage, // Page to write
+   output bit [63:0] data, // array of bytes to write
+   output bit        success);
+
+  endtask: readData
+
+  task writeData
+  // Host sends memPage to thumb drive and then sends data
+  // then returns status to the caller
+  (input  bit [15:0]  mempage, // Page to write
+   input  bit [63:0] data, // array of bytes to write
+   output bit        success);
+
+  endtask: writeData
+
 
 endmodule: usbHost
